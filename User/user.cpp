c4689e26e50c49cca84d2abe2fa6f754bf210c92
御厨　洋 <mikuriya@motionlib.com>
2021-06-23 21:41:39 +0900

    usb loopback

--- User/user.cpp
+++ User/user.cpp
@@ -1,12 +1,13 @@
 /// @file      user.cpp
-/// @author    Hiroshi Mikuriya
-/// @copyright Copyright© 2021 Hiroshi Mikuriya
+/// @author    SATOH GADGET
+/// @copyright Copyright© 2021 SATOH GADGET
 ///
 /// DO NOT USE THIS SOFTWARE WITHOUT THE SOFTWARE LICENSE AGREEMENT.
 
 #include "user.h"
 #include "cmsis_os.h"
 #include "main.h"
+#include "usbd_cdc_if.h"
 
 void StartDefaultTaskImpl(void const *argument)
 {
@@ -15,4 +16,57 @@ void StartDefaultTaskImpl(void const *argument)
         LL_GPIO_TogglePin(LED_ACT_GPIO_Port, LED_ACT_Pin);
         osDelay(500);
     }
+}
+
+void usbRxIRQ(uint8_t const *bytes, uint32_t size)
+{
+    // TODO
+    CDC_Transmit_FS(const_cast<uint8_t *>(bytes), static_cast<uint16_t>(size));
+}
+
+void extiSwIRQ(void)
+{
+    // TODO
+}
+
+void extiSw2IRQ(void)
+{
+    // TODO
+}
+
+void extiMpuIRQ(void)
+{
+    // TODO
+}
+
+void uart1RxIRQ(void)
+{
+    // TODO
+    LL_USART_ReceiveData8(USART1);
+}
+
+void uart1TxEndIRQ(void)
+{
+    // TODO
+}
+
+void uart1TxErrorIRQ(void)
+{
+    // TODO
+}
+
+void uart2RxIRQ(void)
+{
+    // TODO
+    LL_USART_ReceiveData8(USART2);
+}
+
+void uart2TxEndIRQ(void)
+{
+    // TODO
+}
+
+void uart2TxErrorIRQ(void)
+{
+    // TODO
 }
--- User/user.h
+++ User/user.h
@@ -1,19 +1,54 @@
 /// @file      user.h
-/// @author    Hiroshi Mikuriya
-/// @copyright Copyright© 2021 Hiroshi Mikuriya
+/// @author    SATOH GADGET
+/// @copyright Copyright© 2021 SATOH GADGET
 ///
 /// DO NOT USE THIS SOFTWARE WITHOUT THE SOFTWARE LICENSE AGREEMENT.
 
 #pragma once
 
 #ifdef __cplusplus
+#include <cstdint>
 extern "C"
 {
+#else // C99
+#include <stdint.h>
 #endif
 
     /// @brief StartDefaultTask内部処理
     /// @param[in] argument タスク引数
     void StartDefaultTaskImpl(void const *argument);
+
+    /// @brief USB受信割り込み
+    /// @param[in] bytes 受信データの先頭ポインタ
+    /// @param[in] size 受信データサイズ
+    void usbRxIRQ(uint8_t const *bytes, uint32_t size);
+
+    /// @brief GPIO_IN_INT_SW_N 割り込み
+    void extiSwIRQ(void);
+
+    /// @brief GPIO_IN_INT_SW2_N 割り込み
+    void extiSw2IRQ(void);
+
+    /// @brief GPIO_IN_INT_MPU_N 割り込み
+    void extiMpuIRQ(void);
+
+    /// @brief UART1受信割り込み
+    void uart1RxIRQ(void);
+
+    /// @brief UART1 DMA送信完了割り込み
+    void uart1TxEndIRQ(void);
+
+    /// @brief UART1 DMA送信エラー割り込み
+    void uart1TxErrorIRQ(void);
+
+    /// @brief UART2受信割り込み
+    void uart2RxIRQ(void);
+
+    /// @brief UART2 DMA送信完了割り込み
+    void uart2TxEndIRQ(void);
+
+    /// @brief UART2 DMA送信エラー割り込み
+    void uart2TxErrorIRQ(void);
 
 #ifdef __cplusplus
 }
