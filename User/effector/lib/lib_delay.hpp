6110a510ab0cc4adb675ea05cbf21cd50c31f929
--get <mickly9@hotmail.com>
2021-08-06 08:04:41 +0900

    float

--- User/effector/lib/lib_delay.hpp
+++ User/effector/lib/lib_delay.hpp
@@ -8,73 +8,14 @@
 
 #include "common/alloc.hpp"
 #include "constant.h"
+#include "lib_float.hpp"
 #include <algorithm>
 
 namespace satoh
 {
 template <typename T>
 class delayBuf;
-
-template <typename T>
-T fromFloat(float v);
-
-template <typename T>
-float toFloat(T v);
 } // namespace satoh
-
-/// @brief floatからの変換（int8_t版）
-/// @param[in] v -0.1f 〜 0.1f
-/// @return 変換値
-template <>
-inline int8_t satoh::fromFloat<int8_t>(float v)
-{
-  return static_cast<int8_t>(v * 127);
-}
-
-/// @brief floatへの変換（int8_t版）
-/// @param[in] v 値
-/// @return -0.1f 〜 0.1f
-template <>
-inline float satoh::toFloat<int8_t>(int8_t v)
-{
-  return v / 127.0f;
-}
-
-/// @brief floatからの変換（int16_t版）
-/// @param[in] v -0.1f 〜 0.1f
-/// @return 変換値
-template <>
-inline int16_t satoh::fromFloat<int16_t>(float v)
-{
-  return static_cast<int16_t>(v * 32767);
-}
-
-/// @brief floatへの変換（int16_t版）
-/// @param[in] v 値
-/// @return -0.1f 〜 0.1f
-template <>
-inline float satoh::toFloat<int16_t>(int16_t v)
-{
-  return v / 32767.0f;
-}
-
-/// @brief floatからの変換（float版）
-/// @param[in] v -0.1f 〜 0.1f
-/// @return 変換値
-template <>
-inline float satoh::fromFloat<float>(float v)
-{
-  return v;
-}
-
-/// @brief floatへの変換（float版）
-/// @param[in] v 値
-/// @return -0.1f 〜 0.1f
-template <>
-inline float satoh::toFloat<float>(float v)
-{
-  return v;
-}
 
 /// @brief ディレイバッファ
 /// @tparam T 保持するデータ型（int8_t, int16_t or float）
---
+++ User/effector/lib/lib_float.hpp
@@ -0,0 +1,72 @@
+/// @file      effector/lib/lib_float.hpp
+/// @author    SATOH GADGET
+/// @copyright Copyright© 2021 SATOH GADGET
+///
+/// DO NOT USE THIS SOFTWARE WITHOUT THE SOFTWARE LICENSE AGREEMENT.
+
+#pragma once
+
+#include <cstdint>
+
+namespace satoh
+{
+template <typename T>
+T fromFloat(float v);
+
+template <typename T>
+float toFloat(T v);
+} // namespace satoh
+
+/// @brief floatからの変換（int8_t版）
+/// @param[in] v -1.0f 〜 1.0f
+/// @return 変換値
+template <>
+inline int8_t satoh::fromFloat<int8_t>(float v)
+{
+  return static_cast<int8_t>(v * 127);
+}
+
+/// @brief floatへの変換（int8_t版）
+/// @param[in] v 値
+/// @return -1.0f 〜 1.0f
+template <>
+inline float satoh::toFloat<int8_t>(int8_t v)
+{
+  return v / 127.0f;
+}
+
+/// @brief floatからの変換（int16_t版）
+/// @param[in] v -1.0f 〜 1.0f
+/// @return 変換値
+template <>
+inline int16_t satoh::fromFloat<int16_t>(float v)
+{
+  return static_cast<int16_t>(v * 32767);
+}
+
+/// @brief floatへの変換（int16_t版）
+/// @param[in] v 値
+/// @return -1.0f 〜 1.0f
+template <>
+inline float satoh::toFloat<int16_t>(int16_t v)
+{
+  return v / 32767.0f;
+}
+
+/// @brief floatからの変換（float版）
+/// @param[in] v -1.0f 〜 1.0f
+/// @return 変換値
+template <>
+inline float satoh::fromFloat<float>(float v)
+{
+  return v;
+}
+
+/// @brief floatへの変換（float版）
+/// @param[in] v 値
+/// @return -1.0f 〜 1.0f
+template <>
+inline float satoh::toFloat<float>(float v)
+{
+  return v;
+}
